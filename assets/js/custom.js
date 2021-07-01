771a2abb3eb972fae5fbaaef7699f36ebc8eff47
Yota Odaka <amglowez@gmail.com>
2021-07-01 11:22:41 +0900

    cross domain errors

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+// Custom functionality
+jQuery(document).ready(function($){
+	
+	function replaceLogo(){
+		$(".logo-holder .logo.logo-mobile, .logo-holder .logo.logo-dark").attr("src","https://static.mediamath.com/logo/MediaMath-Logo-2021.svg");
+		
+	}
+	replaceLogo();
+	
+});

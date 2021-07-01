4081bf40824fe4065750ecd2fad1621d727c85f1
Yota Odaka <amglowez@gmail.com>
2021-07-01 13:25:34 +0900

    style adjusted

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,10 @@
 // Custom functionality
 jQuery(document).ready(function($){
-	
+
 	function replaceLogo(){
-		$(".logo-holder .logo.logo-mobile, .logo-holder .logo.logo-dark").attr("src","https://static.mediamath.com/logo/MediaMath-Logo-2021.svg");
-		
+		$(".logo-holder .logo.logo-mobile, .logo-holder .logo.logo-dark").attr("src","/assets/img/MediaMath-Logo-2021.svg");
+
 	}
 	replaceLogo();
-	
+
 });

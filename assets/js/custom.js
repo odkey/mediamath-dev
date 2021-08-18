5f08e7d19c8033ef8bd81bd3057db5c9ca4810b9
Yota Odaka <amglowez@gmail.com>
2021-08-18 11:02:25 +0900

    Added new logo

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,10 @@
 // Custom functionality
-jQuery(document).ready(function($){
-
-	function replaceLogo(){
-		$(".logo-holder .logo.logo-mobile, .logo-holder .logo.logo-dark").attr("src","/assets/img/MediaMath-Logo-2021.svg");
-
-	}
-	replaceLogo();
-
-});
+// jQuery(document).ready(function($){
+//
+// 	function replaceLogo(){
+// 		$(".logo-holder .logo.logo-mobile, .logo-holder .logo.logo-dark").attr("src","/assets/img/MediaMath-Logo-2021.svg");
+//
+// 	}
+// 	replaceLogo();
+//
+// });
